0807bae10507bed7756342bd34cf4fc077c340ec
Luke Bradley <bradleyla@g.cofc.edu>
2017-01-31 18:01:11 -0500

    Initial commit

---
+++ app/src/main/java/com/introtoandroid/button_counter_bradley/ButtonCounter.java
@@ -0,0 +1,13 @@
+package com.introtoandroid.button_counter_bradley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ButtonCounter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_counter);
+    }
+}
